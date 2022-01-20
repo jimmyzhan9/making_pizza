6d2c3d8784f9eb69ed9357f89c93d2edbe505246
jimmyzhan9 <jimmyzhang7031@gmail.com>
2022-01-20 16:04:47 +0100

    making pizza

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def pizza_making(size, *toppings):
+    print(f"\nMaking a {size} -inch pizza with the following toppings: ")
+    for topping in toppings:
+        print(f"-{topping}")
+
+def milkshake(size, *toppings):
+    print(f"\n Making a {size} milkshake with the following toppings: ")
+    for topping in toppings:
+        print(f"-{topping}")
+
---
+++ pizza.py
@@ -0,0 +1,6 @@
+from making_pizza.main import *
+
+pizza_making(16, "cheese", "pepperoni")
+pizza_making(18,"cheese", "pepperoni", "galapeggos" )
+milkshake("size 2", "chocolate", "cream", "peanuts")
+milkshake("size 2", "chocolate", "cream", "peanuts", "marshmellows")
